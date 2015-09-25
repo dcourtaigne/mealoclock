3d1dc65eeef6fe8d6ccc2f46570fa8a7f5af6aa6
Audrey92 <odreygarcia@gmail.com>
2015-09-25 14:53:20 +0200

    MAJ footer et style.css

--- app/templates/partials/footer.php
+++ app/templates/partials/footer.php
@@ -1,23 +1,21 @@
       <footer class="bordeauNav">
             <div class="row">
               <div class="col-xs-12 col-sm-4 col-md-4 text-center paddingTB20">
-                      <a href="#" class="color-white"><i id="social-fb" class="fa fa-facebook-square fa-3x social "></i></a>
-                        <a href="#" class="color-white"><i id="social-tw" class="fa fa-twitter-square fa-3x social"></i></a>
-                        <a href="#" class="color-white"><i id="social-em" class="fa fa-envelope-square fa-3x social"></i></a>
-                  </div>
+                  <a href="#" class="color-white"><i id="social-fb" class="fa fa-facebook-square fa-3x social "></i></a>
+                  <a href="#" class="color-white"><i id="social-tw" class="fa fa-twitter-square fa-3x social"></i></a>
+                  <a href="#" class="color-white"><i id="social-em" class="fa fa-envelope-square fa-3x social"></i></a>
+              </div>
                   
-                  <div class="col-xs-12 col-sm-4 col-md-4 color-white text-center paddingTB20">
-                      <article class="text-left">
-                          <p><a href="contact.php">Contact</a></p>
-                          <p>David, Paul, Kevin et Audrey se feront un plaisir de ne pas décrocher le téléphone et de filtrer vos appels au:</p><strong>01.45.46.47.48</strong><br> de 8h à 12h et de 14h à 18h du lundi au samedi
-                      </article>
-                  </div>  
-                  <div class="col-xs-12 col-sm-4 col-md-4 text-left paddingTB20 color-white">   
-                      <p>Mentions légales</p>
-                      <p>Copyright WF3 - 2015</br>
-                      Tous droits réservés.</p>
-
-                  </div>
+              <div class="col-xs-12 col-sm-4 col-md-4 color-white paddingTB20">
+                
+                  <p><a href="contact.php" class="color-white">Contact</a></p>
+                  <p>David, Paul, Kevin et Audrey se feront un plaisir de ne pas décrocher le téléphone et de filtrer vos appels au:</p><strong>01.45.46.47.48</strong><br> de 8h à 12h et de 14h à 18h du lundi au samedi
+                      
+              </div>  
+              <div class="col-xs-12 col-sm-4 col-md-4 paddingTB20 color-white">   
+                  <p>Mentions légales</p>
+                  <p>Copyright DreamTeam - 2015</br>Tous droits réservés.</p>
+              </div>
                   
                   
             </div>
