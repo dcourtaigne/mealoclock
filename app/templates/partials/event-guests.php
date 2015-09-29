f3754dd909cd61db14653165cbab98601d190ce3
Audrey92 <odreygarcia@gmail.com>
2015-09-29 18:35:42 +0200

    Chgt avatar participants

--- app/templates/partials/event-guests.php
+++ app/templates/partials/event-guests.php
@@ -1,2 +1,5 @@
-  <li> <img src="img/chat.png" alt="avatar" class="img-responsive">
-                  <p class="noMargin"><a href="<?= $this->url('userProfile',["id"=>$guest['id']])?>"><?=$this->e($guest['user_name'])?></a></p</li><br>
+  <li> 
+	
+        <img src="<?=$this->assetUrl('img/avatar/chat.png')?>" alt="photo du profil" class="img-responsive thumbnail">
+         
+<p class="noMargin"><a href="<?= $this->url('userProfile',["id"=>$guest['id']])?>"><?=$this->e($guest['user_name'])?></a></p></li><br>
