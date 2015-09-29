42484bce9c012013112e04bbd6ba7ef7594558b3
Paul Herzmann <p.herzmann11@eabjm.org>
2015-09-29 11:40:14 +0200

    modif style css d'affichage de gestion des evenements

--- app/templates/partials/myevents-list.php
+++ app/templates/partials/myevents-list.php
@@ -1,24 +1,22 @@
-<li>
+<li class="center">
   <button class="confirmer_event">Gerer l'evenement</button>
-  <div class="vege col-xs-12 col-sm-12 col-md-12 col-lg-12">
+  <div class="vege col-xs-12">
     <p class=" h4 color-white"><?= $this->e($singleEvent['dateFR'])?></p>
-
   </div>
 
-  <div class="col-xs-12 col-sm-3 col-md-3 col-lg-3 bg-success marginTB20">
-    <h4 class="text-center noMargin"><?= $this->e($singleEvent['time'])?></h4>
+  <div class="col-xs-12 col-sm-3 bg-success marginTB20">
+    <h4 class="noMargin"><?= $this->e($singleEvent['time'])?></h4>
   </div>
 
-  <article>
+  <article class="col-xs-12 col-sm-9">
     <H3>
       <strong><?= $this->e($singleEvent['event_title'])?></strong>
     </H3>
 
-
-    <p class="noMargin chez">
+    <p class="noMargin">
         Chez <a href="#"><?= $this->e($singleEvent['user_name'])?></a> , Paris <?= $this->e($singleEvent['event_location'])?>
     </p>
-    <p class="marginTop20 comment">
+    <p class="marginTop20">
     <?= nl2br($singleEvent['event_desc'])?>
     </p>
   </article>
