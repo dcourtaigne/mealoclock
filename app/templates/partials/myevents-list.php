d3b03b5bf5a74e4fe9daa7617ed484082f0e5ed7
dcourtaigne <dcourtaigne@gmail.com>
2015-09-30 12:50:24 +0200

    ajout bouton modifier event

--- app/templates/partials/myevents-list.php
+++ app/templates/partials/myevents-list.php
@@ -2,16 +2,18 @@
 <li class="center">
 
   <div class="vege col-xs-12">
-    <p class=" h4 color-white"><?= $this->e($singleEvent['dateFR'])?></p> 
+    <p class=" h4 color-white"><?= $this->e($singleEvent['dateFR'])?></p>
     <div class="buttons_gerer_event">
       <button class="confirmer_event" data-id="<?=$singleEvent['id']?>"><a href="<?= $this->url('getEventRequests',['id'=>$singleEvent['id']])?>">Gerer l'evenement</a></button>
+      <button class="update_event"><a href="<?= $this->url('editEvent',['action'=>'edit','id'=>$singleEvent['id']])?>">Modifier l'evenement</a></button>
       <button class="cancel_event">Annuler l'evenement</button>
-    </div> 
+    </div>
     <div class="dropdown_menu">
       <button class="btn btn-primary dropdown-toggle" type="button" data-toggle="dropdown">Options
       <span class="caret"></span></button>
       <ul class="dropdown-menu">
         <li><a href="<?= $this->url('getEventRequests',['id'=>$singleEvent['id']])?>">Gerer l'évenement</a></li>
+        <li><a href="<?= $this->url('editEvent',['action'=>'edit','id'=>$singleEvent['id']])?>">Modifier l'évenement</a></li>
         <li><a href="#">Annuler l'évenement</a></li>
       </ul>
     </div>
